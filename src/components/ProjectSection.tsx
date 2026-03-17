b170079459fc27fb5d9f31316cb313c53d9497f5
Stiven <scopsilver@gmail.com>
2026-03-17 20:59:03 +0700

    second commit

---
+++ src/components/ProjectSection.tsx
@@ -0,0 +1,106 @@
+import { ArrowRight, ExternalLink, GithubIcon } from "lucide-react"
+
+const projects = [
+    {
+        id: 1,
+        title: 'Idlix',
+        description: ' A web film streaming platform. It offers a user-friendly interface for browsing and watching movies and TV shows, with features recommendations and user reviews.',
+        image: '/projects/Idlix.png',
+        tags: ['React', 'Node.js', 'Tailwind CSS', 'MongoDB'],
+        demoUrl: '#',
+        githubUrl: '#'
+    },
+
+    {
+        id: 2,
+        title: 'Loket',
+        description: ' A ticket booking platform. It allows users to browse and book tickets for various events, such as concerts, sports, and theater performances.',
+        image: '/projects/Loket.png',
+        tags: ['React', 'Node.js', 'Tailwind CSS', 'MongoDB'],
+        demoUrl: '#',
+        githubUrl: '#'
+    },
+
+    {
+        id: 3,
+        title: 'Traveloka',
+        description: ' A comprehensive travel booking platform. It allows users to search and book flights, hotels, and other travel services.',
+        image: '/projects/Traveloka.png',
+        tags: ['React', 'Node.js', 'Tailwind CSS', 'MongoDB'],
+        demoUrl: '#',
+        githubUrl: '#'
+    }
+]
+
+export const ProjectSection = () => {
+    return (
+        <section 
+          id="projects" 
+          className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                    Featured <span className="text-primary"> Projects </span>
+                </h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Here are some of the projects I have worked on, showcasing my skills and experience in web development. 
+                    Each project highlights my ability to create responsive, user-friendly applications using modern technologies.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projects.map((project, key) => (
+                        <div 
+                          key={key} 
+                          className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                            <div className="h-48 overflow-hidden">
+                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+                            
+
+                            <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                            <p className="text-muted-foreground text-sm mb-4"> 
+                                {project.description}
+                            </p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a 
+                                      href={project.demoUrl} 
+                                      target="_blank" rel="noopener noreferrer"
+                                      className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <ExternalLink size={20}/>
+                                    </a>
+                                    <a 
+                                      href={project.githubUrl} 
+                                      target="_blank" rel="noopener noreferrer"
+                                      className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <GithubIcon size={20}/>
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="text-center mt-12">
+                    <a 
+                      href="https://github.com/Stiven"
+                      target="_blank" 
+                      className="cosmic-button w-fit flex items-center mx-auto gap-2">
+                        Check My Github <ArrowRight size={16} />
+                    </a>
+                </div>
+
+            </div>
+        </section>
+    )
+}
